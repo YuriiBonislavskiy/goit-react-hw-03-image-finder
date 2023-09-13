222b83f19bdf6df0d8744ec931e3b30c961b7d69
YuriiBonislavskiy <130743571+YuriiBonislavskiy@users.noreply.github.com>
2023-09-14 01:38:22 +0300

    HW 3

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { ColorRing } from 'react-loader-spinner';
+// import { ColorRing } from 'react-loader-spinner';
+import Loader from './Loader';
 import css from './ImageGalleryItem.module.css';
 
 const ImageGalleryItem = ({ webformatURL, tags, id, onSelectedItemView, isLoad }) => {
@@ -16,17 +17,7 @@ const ImageGalleryItem = ({ webformatURL, tags, id, onSelectedItemView, isLoad }
         onLoad={() => !isLoad && setLoading(true)}
       />
       {!loading && !isLoad && (
-        <div className={css.loader}>
-          <ColorRing
-            visible={true}
-            height="200"
-            width="200"
-            ariaLabel="blocks-loading"
-            wrapperStyle={{}}
-            wrapperClass="blocks-wrapper"
-            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-          />
-        </div>
+        <Loader/>
       )}
     </li>
   );
